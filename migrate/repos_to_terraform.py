dece9923068f885558b3758cc8feb0c1915f320a
Feanil Patel <feanil@tcril.org>
2022-09-07 13:36:06 -0400

    feat: Import both public and non-public repos.

--- migrate/repos_to_terraform.py
+++ migrate/repos_to_terraform.py
@@ -5,11 +5,22 @@ import click
 from cache_to_disk import cache_to_disk, delete_disk_caches_for_function
 from ghapi.all import GhApi, paged
 
-REPO_MODULE_TEMPLATE = dedent(
+PUBLIC_REPO_MODULE_TEMPLATE = dedent(
     """
     module "{module_name}" {{
         source = "./modules/repo"
         name = "{repo_name}"
+    }}
+
+    """
+)
+
+NON_PUBLIC_REPO_MODULE_TEMPLATE = dedent(
+    """
+    module "{module_name}" {{
+        source = "./modules/repo"
+        name = "{repo_name}"
+        visibility = "{visibility}"
     }}
 
     """
@@ -39,8 +50,14 @@ REPO_MODULE_TEMPLATE = dedent(
     help="A github personal access token.",
 )
 @click.option(
-    "--modules-file",
+    "--public-repos-file",
     default="../repos.tf",
+    type=click.File("w"),
+    help="The file to write the github repo modules to.",
+)
+@click.option(
+    "--private-repos-file",
+    default="../private.tf",
     type=click.File("w"),
     help="The file to write the github repo modules to.",
 )
@@ -51,20 +68,29 @@ REPO_MODULE_TEMPLATE = dedent(
     help="The file provides the terraform import commands to import the repos.",
 )
 @click.command()
-def main(org, dry_run, refresh_cache, github_token, modules_file, import_commands_file):
+def main(
+    org,
+    dry_run,
+    refresh_cache,
+    github_token,
+    public_repos_file,
+    private_repos_file,
+    import_commands_file,
+):
     api = GhApi()
     repos = chain.from_iterable(paged(api.repos.list_for_org, org, per_page=100))
 
     for repo in repos:
-        if repo.visibility != "public":
-            print(
-                f"Skipping '{repo.name}' because it is '{repo.visibility}' (Not 'public')."
-            )
-            continue
+        if repo.visibility == "public":
+            repos_file = public_repos_file
+            template = PUBLIC_REPO_MODULE_TEMPLATE
+        else:
+            repos_file = private_repos_file
+            template = NON_PUBLIC_REPO_MODULE_TEMPLATE
 
         module_name = repo.name.replace("-", "_").replace(".", "_")
-        modules_file.write(
-            REPO_MODULE_TEMPLATE.format(
+        repos_file.write(
+            template.format(
                 module_name=module_name, repo_name=repo.name, visibility=repo.visibility
             )
         )
